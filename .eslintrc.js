c9cc22b2c7e77524fa7c836de0894bb125b7907a
bridault <oumsouleymenboua@gmail.com>
2021-06-22 22:04:24 +0200

    Bouton radio coché

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,22 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 12
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'double'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    indent: [
+      "warn",
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,35 +35,130 @@ function closeModale(event) {
   modalbg.style.display = "none";
 }
 
-function validate(input) {
-  if (validateData(input)) {
-    //masquer les messages d'erreurs
-    return;
+/**
+ * [validateOnSubmit description]
+ *
+ * @event
+ *
+ * @return  {[type]}         [return description]
+ */
+function validateOnSubmit() {
+  event.preventDefault();
+  event.stopPropagation();
+  inputs.forEach((input) => {
+    if (input.type === "submit" || input.type === "checkbox") return;
+    validate(input);
+  });
+
+  //validation au moins 1 bouton radio coché
+  const quantity = document.getElementById("quantity").value;
+  const inputsLocations = document.querySelectorAll(
+    "#locations input:checked"
+  ).length;
+
+  if (isNaN(quantity) || quantity <= 0){
+    //ajouter des conditions
+    showMessage(document.getElementById("locations"), true);
   }
-  //afficher les messages d'erreur
+  else {
+    console.log("---");
+    let cityError = false;
+    console.log("inputLocations", inputsLocations);
+    if (inputsLocations === 0) {
+      showMessage(
+        document.getElementById("locations"),
+        false,
+        "vous devez choisir au moins une ville"
+      );
+      cityError = true;
+    }
+    if (inputsLocations > quantity && !cityError) {
+      showMessage(
+        document.getElementById("locations"),
+        false,
+        "vous avez choisi plus de villes que de participations"
+      );
+      cityError = true;
+    }
+    if (!cityError) showMessage(document.getElementById("locations"), true);
+  }
+
+  //validation conditions acceptées
+  const checkbox = document.getElementById("checkbox1");
+  if (checkbox.checked) showMessage(checkbox.parentNode, true);
+  else
+    showMessage(
+      checkbox.parentNode,
+      false,
+      "vous devez approver les conditions"
+    );
+
+  console.log(
+    "erreurs",
+    document.querySelectorAll("div[data-error-visible=true]").length
+  );
+  alert("ok");
+}
+
+function validate(input) {
+  if (input.type === "submit") return;
+  const result = validateData(input);
+  showMessage(input.parentNode, result.success, result.msg);
+}
+
+function showMessage(cible, success, message = "") {
+  cible.setAttribute("data-error", message);
+  cible.setAttribute("data-error-visible", !success);
 }
 
 /**
  * [validateData description]
  *
- * @param   {HTMLElement}  input  un Ã©lÃ©ment de type input
+ * @param   {HTMLInputElement}  input  un Ã©lÃ©ment de type input
  *
- * @return  {Boolean}             si le champs est valide ou non
+ * @return  {Object}             un objet avec succes= true|false et si c'est c'est faux un message (msg)
  */
 function validateData(input) {
+  if (input.value.length === 0)
+    return {
+      success: false,
+      msg: "le champs ne peux pas être vide",
+    };
   switch (input.type) {
     case "date":
-      break;
+      return { success: true };
     case "email":
-      console.log(input.checkValidity());
-      break;
+      return input.checkValidity()
+        ? { success: true }
+        : {
+            success: false,
+            msg: "le format de l'email n'est pas valide",
+          };
     case "text":
-      const regex = /[a-z]/gi;
-      if (regex.test(input.value) === false) return false;
-      if (input.value.length < 2) return false;
-      return true;
+      var regex = /[a-z]/gi;
+      if (regex.test(input.value) === false)
+        return {
+          success: false,
+          msg: "le texte saisi n'est pas conforme",
+        };
+      if (input.value.length < 2)
+        return {
+          success: false,
+          msg: "la saisie est trop courte",
+        };
+      return { success: true };
+    case "number":
+      var participations = parseInt(input.value);
+      if (isNaN(participations) || participations < 0)
+        return {
+          success: false,
+          msg: "la saisie est incorrecte",
+        };
+      return { success: true };
+    case "submit":
+      break;
     default:
       console.error(`case ${input.type} non prÃ©vu`);
-      break;
+      return { success: false };
   }
 }
